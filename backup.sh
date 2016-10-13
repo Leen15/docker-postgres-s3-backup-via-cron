3003768a1a9d0b841e44b99eee9cc76b5bba9702
Luca <leen15@msn.com>
2016-10-13 15:38:04 +0200

    add PGDATABASE ENV for export a single database

--- backup.sh
+++ backup.sh
@@ -6,13 +6,16 @@ rm -f /tmp/backup.sql.dump.bz2
 # echo "  - AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}"
 # echo "  - AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
 # echo "  - AWS_BUCKET_NAME=${AWS_BUCKET_NAME}"
-# echo "  - BACKUP_NAME=${BACKUP_NAME}"
+# echo "  - BACKUP_PATH=${BACKUP_PATH}"
 # echo "  - PGHOST=${PGHOST}"
+# echo "  - PGDATABASE=${PGDATABASE}"
 # echo "  - PGPORT=${PGPORT}"
 # echo "  - PGUSER=${PGUSER}"
 # echo "  - PGPASSWORD=${PGPASSWORD}"
 echo "`date` Creating postgres dump"
-pg_dumpall | bzip2 > /tmp/backup.sql.dump.bz2
+
+[ -z "$PGDATABASE" ] && CMD=pg_dumpall || CMD="pg_dump ${PGDATABASE}"
+$CMD | bzip2 > /tmp/backup.sql.dump.bz2
 echo "`date` Uploading to S3"
 /backup/s3upload.rb
 echo "`date` Done!"
